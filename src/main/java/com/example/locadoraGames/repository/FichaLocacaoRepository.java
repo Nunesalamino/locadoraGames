2307e342af178d82b60c35e9df9bde39f2e4f25a
Nunesalamino <nunesalamino@gmail.com>
2025-07-08 21:05:21 -0300

    Create FichaLocacaoRepository.java

---
+++ src/main/java/com/example/locadoraGames/repository/FichaLocacaoRepository.java
@@ -0,0 +1,8 @@
+package com.example.locadoraGames.repository;
+
+import com.example.locadoraGames.model.FichaLocacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FichaLocacaoRepository extends JpaRepository<FichaLocacao , Long> {
+
+}
